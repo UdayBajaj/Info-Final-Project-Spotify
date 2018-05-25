aa092a9ea6ba02e1ee9deaf00f4fb4b467985ee6
Even Whitcomb <evenw@uw.edu>
2018-05-25 16:11:28 -0700

    setup project authentification

--- setup.R
+++ setup.R
@@ -1,3 +1,21 @@
+library("httr")
+library("RCurl") # new package we haven't used before make sure to install
+library("jsonlite")
+
 # This file is for authorizing the spotify API making requests to the API and
 # making calls to the API
 
+# store client_id and client_secret in keys.R file hidden by gitignore
+source("keys.R")
+# format project id for authorization with the API
+id_secret <- base64(paste(client_id, client_secret, sep = ":"))
+
+# authorize project with API
+auth <- POST("https://accounts.spotify.com/api/token",
+     add_headers("Authorization" = paste0("Basic ", id_secret)),
+     body = list(grant_type = "client_credentials"),
+     encode = "form")
+
+auth_body <- content(auth)
+# create token/key for all requests for the API
+token <- body$access_token
